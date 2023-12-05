ef78c6d8d1b0a58ada4dfbb3bcba2eed899267f0
Lucas <89lucasrosa@gmail.com>
2023-12-05 18:32:11 -0300

    Modulo Classe e Composição COncluído

--- oo/carro.py
+++ oo/carro.py
@@ -1,3 +1,6 @@
+
+
+
 """
 
 testando o motor:
@@ -20,9 +23,43 @@ testando o motor:
     >>> motor.frear()
     >>> motor.velocidade
     0
-
-
+    >>> #Testando a direção
+    >>> direcao = Direcao()
+    >>> direcao.valor
+    'Norte'
+    >>> direcao.girar_direita()
+    >>> direcao.valor
+    'Leste'
+    >>> direcao.girar_direita()
+    >>> direcao.valor
+    'Sul'
+    >>> direcao.girar_direita()
+    >>> direcao.valor
+    'Oeste'
+    >>> direcao.girar_direita()
+    >>> direcao.valor
+    'Norte'
+    >>> direcao.girar_esquerda()
+    >>> direcao.valor
+    'Oeste'
+    >>> direcao.girar_esquerda()
+    >>> direcao.valor
+    'Sul'
+    >>> direcao.girar_esquerda()
+    >>> direcao.valor
+    'Leste'
+    >>> direcao.girar_esquerda()
+    >>> direcao.valor
+    'Norte'
+    >>> # Testantando a classe carro
+    carro.
 """
+
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
+
 class Motor:
 
     def __init__(self):
@@ -34,6 +71,22 @@ class Motor:
     def frear(self):
         self.velocidade -= 2
         self.velocidade = max(0, self.velocidade)
+class Direcao:
+    rotacao_direita_dic = {
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+    }
+    rotacao_esquerda_dic = {
+        NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE
+    }
+    def __init__(self):
+        self.valor = NORTE
+    def girar_direita(self):
+        self.valor = self.rotacao_direita_dic[self.valor]
+    def girar_esquerda(self):
+        self.valor = self.rotacao_esquerda_dic[self.valor]
+
+
+
 
 # class Carro:
 #     velocidade = 0
